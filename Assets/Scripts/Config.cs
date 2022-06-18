fac2542b4d4c939d8966e3e35954b6bfeda4566f
DESKTOP-VR7OGIB\ALIENWARE <kivien0511@gmail.com>
2022-06-18 09:15:10 +0800

    card select base

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -9,4 +9,18 @@ public static class Config
     public static float playerInitMoveSpeed = 10f;
 
     public static float screenWidthX = 6f;
+
+    public static int cardListLength = 3;
+
+    public enum CardType{
+        CardType1 = 1,
+        CardType2,
+    }
+
+    public enum CardEffectType{
+        effectType1 = 1,
+        effectType2,
+        effectType3,
+        effectType4
+    }
 }
--- Assets/Scripts/player/Player.cs
+++ Assets/Scripts/player/Player.cs
@@ -3,16 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public GameObject cardUI;
+    public List<Card> cardList;
+    private int cardListLength;
+    private int cardIndex = -1;
 
     public int hp = Config.playerInitHp;
 
     // Start is called before the first frame update
     void Start()
     {
+        cardList = new List<Card>();
+        cardListLength = Config.cardListLength;
         print(hp);
     }
 
@@ -20,24 +27,9 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
-        checkOverEdge();
+        CheckOverEdge();
+        SelectCard();
     }
-
-    // private void OnCollisionEnter2D(Collision2D col) {
-    //     //下落的时候才计算碰撞。不然角色只要碰到东西就会往上飞
-    //     if (rb.velocity.y <= 0)
-    //     {
-    //         if (col.CompareTag("board"))
-    //         {
-    //             rb.velocity = new Vector2(0f, 10f);
-    //         }
-    //     }
-
-    //     if (col.CompareTag("win"))
-    //     {
-    //         SceneManager.LoadScene("Win");
-    //     }
-    // }
        
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -68,10 +60,9 @@ public class Player : MonoBehaviour
         }
 
         rb.velocity = new Vector2(horizontalAxis * Config.playerInitMoveSpeed, rb.velocity.y);
-        print(rb.velocity);
     }
 
-    void checkOverEdge() {
+    void CheckOverEdge() {
         if (transform.position.x < -Config.screenWidthX) {
             transform.position = new Vector2(6f, transform.position.y);
         }
@@ -80,4 +71,72 @@ public class Player : MonoBehaviour
         }
     }
 
+    void SelectCard() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (cardIndex == -1) {
+                print("not select card");
+                return;
+            }
+            cardIndex = -1;
+            print("use card");
+        }
+
+ 
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            print("select card 1");
+            cardIndex = 0;
+            for (int i = 0; i < cardListLength; i ++) {
+                cardUI.transform.GetChild(i).gameObject.GetComponent<Image>().color = Color.white;
+            }
+            cardUI.transform.GetChild(cardIndex).gameObject.GetComponent<Image>().color = Color.red;
+        } else if (Input.GetKeyDown(KeyCode.W)) {
+            print("select card 2");
+            cardIndex = 1;
+            for (int i = 0; i < cardListLength; i ++) {
+                cardUI.transform.GetChild(i).gameObject.GetComponent<Image>().color = Color.white;
+            }
+             cardUI.transform.GetChild(cardIndex).gameObject.GetComponent<Image>().color = Color.red;
+        } else if (Input.GetKeyDown(KeyCode.E)) {
+            print("select card 3");
+            for (int i = 0; i < cardListLength; i ++) {
+                cardUI.transform.GetChild(i).gameObject.GetComponent<Image>().color = Color.white;
+            }
+            cardIndex = 2;
+            cardUI.transform.GetChild(cardIndex).gameObject.GetComponent<Image>().color = Color.red;
+        }
+
+        if (cardIndex != -1) {
+            if (cardList.Count <= cardIndex) {
+                return;
+            }
+            Card targetCard = cardList[cardIndex];
+            if (targetCard == null) {
+                print("not targetCard");
+                cardIndex = -1;
+                return;
+            }
+        }
+
+    }
+
+}
+
+public class Card {
+
+    public int cardType;
+    public int cardEffectType;
+
+    public Card() {
+
+    }
+
+    void Init() {
+
+    }
+
+    void InitTest() {
+        cardType = (int)Config.CardType.CardType1;
+        cardEffectType = (int)Config.CardEffectType.effectType1;
+    }
 }
