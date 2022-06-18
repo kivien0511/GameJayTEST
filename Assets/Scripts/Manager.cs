bc407a7f5f30dc11028d2fb2324e485fbdedd490
DESKTOP-VR7OGIB\ALIENWARE <kivien0511@gmail.com>
2022-06-18 23:15:50 +0800

    change

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 public class Manager : MonoBehaviour
 {
     public GameObject platformPrefab;
+    public GameObject specialPlatformPrefab1;
+    public GameObject specialPlatformPrefab2;
     public int numberOfPlatforms;
     public GameObject winPointPrefab;
     public float levelWidth = 3f;
@@ -14,9 +16,14 @@ public class Manager : MonoBehaviour
     public float maxY = 1.8f;
     private float lastx;
 
+    public List<GameObject> specialPlatformPrefabList;
+
+    private int flag = 1; // 初始为正
+
     private void Start()
     {
         Vector3 spawnPosition = new Vector3();
+        specialPlatformPrefabList = new List<GameObject>();
         for (int i = 0; i < numberOfPlatforms; i++)
         {
             spawnPosition.y += UnityEngine.Random.Range(minY, maxY);
@@ -36,8 +43,38 @@ public class Manager : MonoBehaviour
             }
             else
             {
-                Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+                int rand = UnityEngine.Random.Range(1, 11);
+                if (rand < 6) {
+                    Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+                } else if (rand > 6 && rand <= 8) {
+                    GameObject obj = Instantiate(specialPlatformPrefab1, spawnPosition, Quaternion.identity);
+                    specialPlatformPrefabList.Add(obj);
+                } else if (rand > 8 && rand <= 10) {
+                    GameObject obj = Instantiate(specialPlatformPrefab2, spawnPosition, Quaternion.identity);
+                    specialPlatformPrefabList.Add(obj);
+                }
             }
+        }
+    }
+
+    private void Update() {
+        if (flag != Player.flag) {
+            // 刷新特殊地板正反
+
+            if (Player.flag == 1) {
+                foreach (var x in specialPlatformPrefabList) {
+                    if (x == null) continue;
+                    x.transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
+                }
+            }
+            if (Player.flag == -1) {
+                foreach (var x in specialPlatformPrefabList) {
+                    if (x == null) continue;
+                    x.transform.rotation = Quaternion.Euler(new Vector3(0,0,-180));
+                }
+            }
+
+            flag = Player.flag;
         }
     }
 }
--- Assets/Scripts/player/Player.cs
+++ Assets/Scripts/player/Player.cs
@@ -9,15 +9,26 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
     public GameObject cardUI;
+    public GameObject hpUI;
+    public GameObject hpUIimage;
     public List<Card> cardList;
     private int cardListLength;
     private int cardIndex = -1;
 
+    private bool boardSpeedFlag = false;
+    private int boardSpeed = 0;
+
+    private float timer = 0f;
+
+    public static int flag = 1; // 正
+
     public int hp = Config.playerInitHp;
+    public int hpMax = Config.playerInitHp;
 
     // Start is called before the first frame update
     void Start()
     {
+        DrawHpUI();
         cardList = new List<Card>();
         cardListLength = Config.cardListLength;
         print(hp);
@@ -26,6 +37,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (boardSpeedFlag) {
+            timer += Time.deltaTime;
+            if (timer >= 3f) {
+                timer = 0f;
+                boardSpeedFlag = false;
+            }
+        }
+
         Move();
         CheckOverEdge();
         SelectCard();
@@ -39,6 +58,35 @@ public class Player : MonoBehaviour
             if (col.CompareTag("board"))
             {
                 rb.velocity = new Vector2(0f, 10f);
+
+                if (col.gameObject.name.Contains("1")) {
+                    // 特殊地板1
+                    // print("特殊地板1");
+                    // 正 - 1 ， 负 + 1
+                    if (flag > 0) {
+                        if (hp - 1 > 0) {
+                            hp -= 1;
+                        } else if (hp - 1 == 0) {
+                            print("dead");
+                        }
+                    } else if (flag < 0) {
+                        if (hp + 1 >= hpMax) {
+                            hp = hpMax;
+                        } else if (hp + 1 < hpMax) {
+                            hp += 1;
+                        }
+                    }
+                    DrawHpUI();
+                    // print("hp:"+hp);
+                } else if (col.gameObject.name.Contains("2")) {
+                    // 特殊地板2
+                    // print("特殊地板2");
+                    // 正 + 移速 ， 负 - 移速
+                    boardSpeed = 0;
+                    boardSpeedFlag = true;
+                } 
+
+                Destroy(col.gameObject);
             }
         }
 
@@ -49,6 +97,14 @@ public class Player : MonoBehaviour
     }
 
     void Move() {
+        // print(flag);
+        if (Input.GetKeyDown(KeyCode.R)) { 
+            flag = 1;
+        } 
+        if (Input.GetKeyDown(KeyCode.F)) { 
+            flag = -1;
+        }
+
         float horizontalAxis = 0;
         horizontalAxis = Input.GetAxis("Horizontal");
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
@@ -57,6 +113,26 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
             transform.rotation = Quaternion.Euler(new Vector3(0,-180,0));
+        }
+
+        if (flag == 1) { // 正
+            transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
+        }
+
+        if (flag == -1) { // 反
+            transform.rotation = Quaternion.Euler(new Vector3(0,0,-180));
+        }
+
+        if (boardSpeed == 0) {
+            boardSpeed = 3; // 根据卡牌计算
+
+            boardSpeed *= flag;
+        }
+
+        if (boardSpeedFlag) {
+            print("boardSpeed:"+boardSpeed);
+            rb.velocity = new Vector2(horizontalAxis * (Config.playerInitMoveSpeed + boardSpeed), rb.velocity.y);
+            return;
         }
 
         rb.velocity = new Vector2(horizontalAxis * Config.playerInitMoveSpeed, rb.velocity.y);
@@ -80,8 +156,6 @@ public class Player : MonoBehaviour
             cardIndex = -1;
             print("use card");
         }
-
- 
 
         if (Input.GetKeyDown(KeyCode.Q)) {
             print("select card 1");
@@ -118,6 +192,26 @@ public class Player : MonoBehaviour
             }
         }
 
+    }
+
+    void DrawHpUI() {
+        for (int i = 0; i < hpUI.transform.childCount; i ++) {
+            Destroy(hpUI.transform.GetChild(i).gameObject);
+        }
+
+        for (int i = 0; i < hp; i ++) {
+            GameObject obj = Instantiate(hpUIimage, Vector3.zero, Quaternion.identity);
+            obj.transform.position = hpUI.transform.position + new Vector3(i * 100, 0 ,0);
+            obj.transform.SetParent(hpUI.transform);
+        }
+    }
+
+    void FallSpeedUp() {
+        if (rb.velocity.y < 0) {
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+                
+            }   
+        }
     }
 
 }
